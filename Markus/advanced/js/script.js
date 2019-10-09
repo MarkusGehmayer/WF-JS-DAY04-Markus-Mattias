c3d82a9863dc3292e1744d3aa7a8318b6581235c
Markus Gehmayer <markus.gehmayer@gmx.at>
2019-10-09 15:51:50 +0200

    finally

--- Markus/advanced/js/script.js
+++ Markus/advanced/js/script.js
@@ -11,35 +11,27 @@ for(var i = 0; i < book.length; i++) {
 // UL, LI
   var ulist = document.createElement("ul");
   var li = document.createElement("li");
+  li.setAttribute("id","farbe"+i)
+  
   var pic = document.createElement("IMG");
+  pic.setAttribute("src", "./img/book"+[i]+".png");
+  pic.setAttribute("width", "120");
+  pic.setAttribute("height", "150");
+
   li.appendChild(pic);
   ulist.appendChild(li);
   var elementUL = document.getElementById("main");
   elementUL.appendChild(ulist);
 
 // Picture
-  pic.setAttribute("src", "./img/book.png");
-  pic.setAttribute("width", "120");
-  pic.setAttribute("height", "150");
-  document.body.appendChild(pic);
-
-  };
-
-
-
-
-  // var row = table.insertRow(i);
-  // var cell1 = row.insertCell(0);
-  // var cell2 = row.insertCell(1);
-  // var cell3 = row.insertCell(2);
-  // var cell4 = row.insertCell(3);
-  // var cell5 = row.insertCell(4);
-  // var cell6 = row.insertCell(5);
-  // cell1.innerHTML = obj.Unique_ID;
-  // cell2.innerHTML = obj.First_Name;
-  // cell3.innerHTML = obj.Last_Name;
-  // cell4.innerHTML = obj.Email_address;
-  // cell5.innerHTML = obj.Job_Title;
-  // cell6.innerHTML = obj.Salary;
-
+  var elementPIC = document.getElementById("main");
+  elementPIC.appendChild(li);
+  
+// BOOLEAN
+if (obj.read == "true") {
+  document.getElementById("farbe"+i).style.backgroundColor = "green";
+} else {
+  document.getElementById("farbe"+i).style.backgroundColor = "red";
+}
+};
 };
